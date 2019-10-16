29cdc7c88bad3678683528b22522167ad92efa0b
Michał Świętochowski <m.swietochowski@insee.pl>
2019-10-16 14:09:56 +0200

    Downgrade symfony cli version to 4.7.2

--- Formula/symfony-cli.rb
+++ Formula/symfony-cli.rb
@@ -1,9 +1,9 @@
 class SymfonyCli < Formula
   desc "Installs the Symfony CLI"
   homepage "https://symfony.com/"
-  url "https://github.com/symfony/cli/releases/download/v4.7.3/symfony_darwin_amd64.gz"
-  version "4.7.3"
-  sha256 "6ab4ee9620674dfa47fbd223137cd1ceff80f1ddfa0ff2db688ecec8c85db7df"
+  url "https://github.com/symfony/cli/releases/download/v4.7.2/symfony_darwin_amd64.gz"
+  version "4.7.2"
+  sha256 "f250c5c6497f5f56e2359c1311238a3020123d02ea4a0aa39bbb1dcf0c2fe4dc"
 
   depends_on "php"
   depends_on "composer"
